876f02658e33fbf3bdf83c01555ec4f9f064e77b
lanlan9977 <lanlan9977@gmail.com>
2019-02-22 22:13:10 +0800

    TEST

--- app/src/main/java/idv/david/foodgodapp/OrderActivity.java
+++ app/src/main/java/idv/david/foodgodapp/OrderActivity.java
@@ -1,5 +1,10 @@
 package idv.david.foodgodapp;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.TabLayout;
@@ -8,15 +13,27 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
@@ -29,60 +46,121 @@ import idv.david.foodgodapp.order.fragment.MenuOrderFragment;
 
 //(訂單專區)
 public class OrderActivity extends AppCompatActivity {
+
     private BottomSheetBehavior bottomSheetBehavior;
     private TextView textView;
     private boolean isOnClick;
     private Spinner reviewStauts;
+    public static List<MenuOrder> menuOrderList;
+    private final static String TAG = "OrderActivity";
+    private AsyncTask retrieveMenuOrderTask;
 
 
+    private class RetrieveMenuOrderTask extends AsyncTask<String, String, List<MenuOrder>> {
+        private ProgressDialog progressDialog;
+        private final static String TAG = "OrderActivity";
 
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(OrderActivity.this);
+            progressDialog.setMessage("Loading...");
+            progressDialog.show();
+        }
+
+        @Override
+        protected List<MenuOrder> doInBackground(String... params) {
+            String url = params[0];
+            String jsonIn;
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("param", "menuOredrList");
+            jsonIn = getRemoteData(url, jsonObject.toString());
+            Gson gson = new Gson();
+            Type listType = new TypeToken<List<MenuOrder>>() {
+            }.getType();
+            return gson.fromJson(jsonIn, listType);
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(List<MenuOrder> items) {
+            menuOrderList = new ArrayList<>();
+            menuOrderList = items;
+            ViewPager viewPager = findViewById(R.id.viewPager);
+            viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));//頁面手勢滑動
+            TabLayout tabLayout = findViewById(R.id.tabLayout);//訂單種類滑動列表
+            viewPager.isFakeDragging();
+            tabLayout.setupWithViewPager(viewPager);
+            progressDialog.cancel();
+        }
+
+
+    }
+
+    private String getRemoteData(String url, String outStr) {
+        HttpURLConnection connection = null;
+        StringBuilder inStr = new StringBuilder();
+        try {
+            connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setDoInput(true); // allow inputs
+            connection.setDoOutput(true); // allow outputs
+            // 不知道請求內容大小時可以呼叫此方法將請求內容分段傳輸，設定0代表使用預設大小
+            connection.setChunkedStreamingMode(0);
+            connection.setUseCaches(false); // do not use a cached copy
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("charset", "UTF-8");
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
+            bw.write(outStr);
+            Log.d(TAG, "output: " + outStr);
+            bw.close();
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == 200) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                while ((line = br.readLine()) != null) {
+                    inStr.append(line);
+                }
+            } else {
+                Log.d(TAG, "response code: " + responseCode);
+            }
+        } catch (IOException e) {
+            Log.e(TAG, e.toString());
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+        Log.d(TAG, "input: " + inStr);
+        return inStr.toString();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
-
-
-        View bottomSheet = findViewById(R.id.bottom_sheet);//下方訂單內容
-        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
-
-
-        ViewPager viewPager = findViewById(R.id.viewPager);
-        viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));//頁面手勢滑動
-
-        TabLayout tabLayout = findViewById(R.id.tabLayout);//訂單種類滑動列表
-        viewPager.isFakeDragging();
-        tabLayout.setupWithViewPager(viewPager);
-
-        MenuOrderDisplay();//展示下方訂單內容方法
+        if (networkConnected()) {
+            retrieveMenuOrderTask = new RetrieveMenuOrderTask().execute(Util.URL);
+        } else {
+            Toast.makeText(OrderActivity.this, "網路連線錯誤", Toast.LENGTH_SHORT).show();
+        }
         findViews();
-
-
-
     }
-
-
 
     private void findViews() {
         reviewStauts = findViewById(R.id.idReviewStatus);
-        String[] stringStuts = {"請審核", "審核通過", "審核未通過"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>
-                (this, android.R.layout.simple_spinner_item, stringStuts);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        reviewStauts.setAdapter(adapter);
-        reviewStauts.setSelection(0, true);
-//        reviewStauts.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            @Override
-//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//
-//            }
-//
-//            @Override
-//            public void onNothingSelected(AdapterView<?> parent) {
-//
-//            }
-//        });
+        View bottomSheet = findViewById(R.id.bottom_sheet);//下方訂單內容
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+    }
 
+    private boolean networkConnected() {
+        ConnectivityManager conManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = conManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
     }
 
     private void MenuOrderDisplay() {
@@ -108,14 +186,16 @@ public class OrderActivity extends AppCompatActivity {
         textView.setText(sb);
     }
 
-    public void orderClick(View view) {
-        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+    public void onOrderClick(View view ) {
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        bottomSheetBehavior.setPeekHeight(985);
         if (!isOnClick) {
             isOnClick = true;
         } else {
             bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
             isOnClick = false;
         }
+        MenuOrderDisplay();//展示下方訂單內容方法
     }
 
     private class MyPagerAdapter extends FragmentPagerAdapter {
--- app/src/main/java/idv/david/foodgodapp/order/fragment/MenuOrderFragment.java
+++ app/src/main/java/idv/david/foodgodapp/order/fragment/MenuOrderFragment.java
@@ -1,86 +1,41 @@
 package idv.david.foodgodapp.order.fragment;
 
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.reflect.TypeToken;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.lang.reflect.Type;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 import idv.david.foodgodapp.MenuOrder;
 import idv.david.foodgodapp.OrderActivity;
 import idv.david.foodgodapp.R;
-import idv.david.foodgodapp.Util;
-
-import static android.support.v4.content.ContextCompat.getSystemService;
 
 public class MenuOrderFragment extends Fragment {
 
-    private List<MenuOrder> MenuOrderList;
-    private AsyncTask retrieveMenuOrderTask;
-    private
+    private List<MenuOrder> menuOrderList;
+    private LinearLayout idMenu_Order_Layout;
+
 
     public MenuOrderFragment() {
-//        利用建構子帶入MENU_ORDER VO
-        MenuOrderList = new ArrayList<>();
-        MenuOrder menuOrder = new MenuOrder("M02091", "M1", new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis()), new Date(), 0, "", "C00001", "C00001", "M00001");
-
-        MenuOrderList.add(menuOrder);
+        menuOrderList = OrderActivity.menuOrderList;
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
-        final View view = inflater.inflate(R.layout.fragment_menuorder, container, false);
-
+        View view = inflater.inflate(R.layout.fragment_menuorder, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.menuOrderView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         recyclerView.setAdapter(new MenuOrderAdapter(inflater));
-
-        if (networkConnected()) {
-            retrieveMenuOrderTask = new RetrieveMenuOrderTask().execute(Util.URL);
-        } else {
-            Toast.makeText(this, "沒有網路", Toast.LENGTH_SHORT).show();
-        }
-
         return view;
     }
-    private boolean networkConnected() {
-        ConnectivityManager conManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = conManager.getActiveNetworkInfo();
-        return networkInfo != null && networkInfo.isConnected();
-    }
-
-
-
 
 
     private class MenuOrderAdapter extends RecyclerView.Adapter<MenuOrderAdapter.ViewHolder> {
@@ -91,7 +46,8 @@ public class MenuOrderFragment extends Fragment {
         }
 
         class ViewHolder extends RecyclerView.ViewHolder {
-            TextView idMenu_or_id, idMenu_or_appt;
+            private TextView idMenu_or_id, idMenu_or_appt;
+
 
             public ViewHolder(View itemView) {
                 super(itemView);
@@ -104,113 +60,26 @@ public class MenuOrderFragment extends Fragment {
         public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             View itemView = inflater.inflate(R.layout.card_menuorder, parent, false);
             ViewHolder viewHolder = new ViewHolder(itemView);
+            idMenu_Order_Layout= itemView.findViewById(R.id.idMenu_Order_Layout);
             return viewHolder;
         }
 
         @Override
         public void onBindViewHolder(ViewHolder viewHolder, int position) {
-            final MenuOrder MenuOrder = MenuOrderList.get(position);
-            viewHolder.idMenu_or_id.setText(MenuOrder. getMenu_ID());
-            viewHolder.idMenu_or_appt.setText((MenuOrder.getMenu_od_book()).toString());
-
-            viewHolder.itemView.setOnClickListener(new OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                }
-            });
+            MenuOrder menuOrder=menuOrderList.get(position);
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy 年 MM 月 dd 日 HH : mm ");
+            viewHolder.idMenu_or_id.setText("訂單編號："+menuOrder.getMenu_od_ID());
+            viewHolder.idMenu_or_appt.setText("預約日期："+sdf.format(menuOrder.getMenu_od_book()));
+            if("g0".equals(menuOrder.getMenu_od_status()))
+            idMenu_Order_Layout.setBackgroundColor(getResources().getColor(R.color.colorRed));
         }
-
         @Override
         public int getItemCount() {
-            return MenuOrderList.size();
+            return menuOrderList.size();
         }
 
 
     }
-
-
-
-
-
-
-
-
-
-
-    private class RetrieveMenuOrderTask extends AsyncTask<String, String, List<String>> {
-        private ProgressDialog progressDialog;
-        private final static String TAG="OrderActivity";
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            progressDialog = new ProgressDialog(OrderActivity.this);
-            progressDialog.setMessage("Loading...");
-            progressDialog.show();
-        }
-
-        @Override
-        protected List<String> doInBackground(String... params) {
-            String url = params[0];
-            String jsonIn;
-            JsonObject jsonObject = new JsonObject();
-            jsonObject.addProperty("param", "area");
-            jsonIn = getRemoteData(url, jsonObject.toString());
-            Gson gson = new Gson();
-            Type listType = new TypeToken<List<String>>() {
-            }.getType();
-
-            return gson.fromJson(jsonIn,listType);
-        }
-
-        @Override
-        protected void onPostExecute(List<String> items) {
-            ArrayAdapter<String> adapter=new ArrayAdapter<>(OrderActivity.this,android.R.layout.simple_list_item_1,items);
-            adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-            reviewStauts.setAdapter(adapter);
-            progressDialog.cancel();
-
-
-        }
-
-        private String getRemoteData(String url, String outStr) {
-            HttpURLConnection connection = null;
-            StringBuilder inStr = new StringBuilder();
-            try {
-                connection = (HttpURLConnection) new URL(url).openConnection();
-                connection.setDoInput(true); // allow inputs
-                connection.setDoOutput(true); // allow outputs
-                // 不知道請求內容大小時可以呼叫此方法將請求內容分段傳輸，設定0代表使用預設大小
-                connection.setChunkedStreamingMode(0);
-                connection.setUseCaches(false); // do not use a cached copy
-                connection.setRequestMethod("POST");
-                connection.setRequestProperty("charset", "UTF-8");
-                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
-                bw.write(outStr);
-                Log.d(TAG, "output: " + outStr);
-                bw.close();
-
-                int responseCode = connection.getResponseCode();
-                if (responseCode == 200) {
-                    BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                    String line;
-                    while ((line = br.readLine()) != null) {
-                        inStr.append(line);
-                    }
-                } else {
-                    Log.d(TAG, "response code: " + responseCode);
-                }
-            } catch (IOException e) {
-                Log.e(TAG, e.toString());
-            } finally {
-                if (connection != null) {
-                    connection.disconnect();
-                }
-            }
-            Log.d(TAG, "input: " + inStr);
-            return inStr.toString();
-        }
 
 
 }
